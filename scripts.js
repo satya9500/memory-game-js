3c7a584dfabbf63049820c392b3937de21ca5ca8
satya9500 <satya.prakash9500@gmail.com>
2019-12-31 23:50:46 +0530

    board locked after two flips

--- scripts.js
+++ scripts.js
@@ -2,6 +2,33 @@ const cards = document.querySelectorAll(".memory-card");
 
 cards.forEach(card => card.addEventListener("click", flipCard));
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
 function flipCard() {
+  if (lockBoard) return;
   this.classList.toggle("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+  }
+}
+function checkForMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  } else {
+    lockBoard = true;
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      lockBoard = false;
+    }, 1500);
+  }
 }
